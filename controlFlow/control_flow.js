427eca788a9d58c27fb2a31e0adce5e567b52dd4
Phyllis-006 <phyllisciru06@gmail.com>
2025-07-03 02:19:09 -0400

    new changes

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -40,4 +40,8 @@ switch (userType){
     default:
         userCategory = "Unknown";
 }
-console.log("User Category:", userCategory)
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
